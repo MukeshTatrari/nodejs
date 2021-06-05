f0e673f8a67802c4f7448db863a4c8e711f870a3
Chandra <Mukesh.Chandra@bsci.com>
2021-06-05 20:44:30 +0530

    blogs example

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const fs = require("fs");
+const _ = require("lodash");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  res.setHeader("Content-Type", "text/html");
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-us":
+      res.statusCode = 301;
+      res.setHeader('Location','/about')
+      res.end()
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (error, data) => {
+    if (!error) {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listening on port 30000");
+});
